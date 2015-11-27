3148fa36e89b6a2f1aca279bc9c118fb8dadff7f
Diogo Redin <diogo.redin@gmail.com>
2015-11-27 13:32:55 +0000

    nova versao

--- main.py
+++ main.py
@@ -55,16 +55,18 @@ def coordenada_coluna(coord):
 ############################################
 #
 # Funcoes Tabuleiro:						   
-# 	- Cria Tabuleiro (cria_tabuleiro)    
+# 	- Cria Tabuleiro (cria_tabuleiro)
+#	- Dimensoes Tabuleiro (tabuleiro_dimensoes)  
+#	- Especificacoes Tabuleiro (tabuleiro_especificacoes
 #
 ############################################
 
+############################################
+# CRIA TABULEIRO				               
+############################################
+
 def cria_tabuleiro(t):
-	
-	# Testa se recebe dois tuplos
-	for tuplos in t:
-		list[tuplos] = isinstance(t[tuplos], (tuple))
-		
+
 	# Testa o tuplo
 	if not( isinstance(t, (tuple))
 			# se o tuplo contem dois tuplos
@@ -72,11 +74,41 @@ def cria_tabuleiro(t):
 			# se esses tuplos nao sao vazios
 			and all( len(tuplos) > 0 for tuplos in t )
 			# se as especificacoes sao positivas
-			and all( elem > 0 for elem in espec for espec in tuplos for tuplos in t )
+			and all( elem > 0 for tuplos in t for espec in tuplos for elem in espec )
 			# se as especificacoes sao inteiras
-			and all( isinstance(elem, (int)) for elem in espec for espec in tuplos for tuplos in t )
+			and all( isinstance(elem, (int)) for tuplos in t for espec in tuplos for elem in espec ) ):
 			
 		raise ValueError('cria_tabuleiro: argumentos invalidos')
-		
-	# Devolve o tabuleiro num dicionario (com as especificacoes dentro de tuplos)
-	return {'linhas' : t[0], 'colunas' : t[1]}
+	
+	# Qtd linhas & colunas
+	linhas = tabuleiro_dimensoes([0])
+	colunas = tabuleiro_dimensoes([1])
+	
+	# Devolve o tabuleiro num dicionario
+	return {'linhas' : t[0], 'colunas' : t[1] 'celulas' : [[[0]*linhas]*colunas]}
+
+############################################
+# TABULEIRO DIMENSOES		               
+############################################
+
+def tabuleiro_dimensoes(tab):
+
+	# Qtd linhas & colunas
+	linhas = len(tab['linhas'])
+	colunas = len(tab['colunas'])
+	
+	# Devolve dimensoes num tuplo
+	return (linhas, colunas)
+
+############################################
+# TABULEIRO ESPECIFICACOES		               
+############################################
+
+def tabuleiro_especificacoes(tab):
+
+	# Qtd linhas & colunas
+	linhas = tab['linhas']
+	colunas = tab['colunas']
+	
+	# Devolve dimensoes num tuplo
+	return (linhas, colunas)
