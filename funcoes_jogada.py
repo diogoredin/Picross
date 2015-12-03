496287844896ca4e93bdc696d918afdba2d2c341
Diogo Redin <diogo.redin@gmail.com>
2015-12-03 17:04:53 +0000

    Inicio

---
+++ funcoes_jogada.py
@@ -0,0 +1,27 @@
+####################################################
+#
+#	CLASSE JOAGADA
+#
+####################################################
+
+class jogada:
+
+    def __init__ (self, l, c):
+
+        # Testa se são inteiros positivos
+        if not(isinstance(l, (int)) and isinstance(c, (int)) and (l > 0) and (c > 0)):
+            raise ValueError('cria_jogada: argumentos invalidos')
+        self.linha = l
+        self.coluna = c
+
+def cria_jogada:
+
+def jogada_coordenada:
+
+def jogada_valor:
+
+def e_jogada:
+
+def jogadas_iguais:
+
+def jogada_para_cadeia:
