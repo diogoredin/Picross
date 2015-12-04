03c65223ae57528168257b4604ae848040336cb4
diogofsvilela <diogofsvilela@gmail.com>
2015-12-04 17:43:09 +0000

    funcoes_coordenada com fix nos objetos

--- funcoes_coordenada.py
+++ funcoes_coordenada.py
@@ -2,32 +2,32 @@ class coordenada:
 
     def __init__ (self, l, c):
 
-        # Testa se são inteiros positivos
+        # Testa se sao inteiros positivos
         if not(isinstance(l, (int)) and isinstance(c, (int)) and (l > 0) and (c > 0)):
             raise ValueError('cria_coordenada: argumentos invalidos')
         self.linha = l
         self.coluna = c
 
-def cria_coordenada(linha, coluna):
-    return coordenada (linha, coluna)
+def cria_coordenada(l, c):    
+    return coordenada (l, c)
 
-def coordenada_linha(coord):
-    return coordenada.linha(coord)
+def coordenada_linha(c):
+    return c.linha()
 
-def coordenada_coluna(coord):
-    return coordenada.coluna(coord)
+def coordenada_coluna(c):
+    return c.coluna()
 
-def e_coordenada(elemento):
-    return isinstance(elemento, coordenada)
+def e_coordenada(e):
+    return isinstance(e, coordenada)
 
-def coordenadas_iguais(coord1, coord2):
-    linha1 = coordenada_linha(coord1)
-    coluna1 = coordenada_coluna(coord1)
-    linha2 = coordenada_linha(coord2)
-    coluna2 = coordenada_coluna(coord2)
+def coordenadas_iguais(c1, c2):
+    linha1 = coordenada_linha(c1)
+    coluna1 = coordenada_coluna(c1)
+    linha2 = coordenada_linha(c2)
+    coluna2 = coordenada_coluna(c2)
     return (linha1 == linha2 and coluna1 == coluna2)
 
-def coordenada_para_cadeia(coord):
-    linha = str(coordenada_linha(coord))
-    coluna = str(coordenada_coluna(coord))
+def coordenada_para_cadeia(c):
+    linha = str(coordenada_linha(c))
+    coluna = str(coordenada_coluna(c))
     return '(' + linha + ' : ' + coluna + ')'
