656f52ebd6505c527387756ee74ee205acd37942
diogofsvilela <diogofsvilela@gmail.com>
2015-12-05 22:44:20 +0000

    atualizacao duncoes_tabuleiro.py

--- funcoes_tabuleiro.py
+++ funcoes_tabuleiro.py
@@ -1,94 +1,215 @@
 class tabuleiro:
     def __init__(self, t):
-        # se o argumento e um tuplo
+        # Se o argumento e um tuplo
         if not( isinstance(t, (tuple))
-                # se o tuplo contem dois tuplos
+                # Se o tuplo contem dois tuplos
                 and len(t) == 2
-                # se os elementos do tuplo sao tuplos
+                # Se os elementos do tuplo sao tuplos
                 and all( isinstance(tuplos, (tuple)) for tuplos in t )
-                # se esses tuplos nao sao vazios
+                # Se esses tuplos nao sao vazios
                 and all( len(tuplos) > 0 for tuplos in t )
-                # se as especificacoes sao inteiras
+                # Se as especificacoes sao inteiras
                 and all( isinstance(elem, (int)) for tuplos in t for espec in tuplos for elem in espec ) 
-                # se as especificacoes sao positivas
+                # Se as especificacoes sao positivas
                 and all( (elem > 0) for tuplos in t for espec in tuplos for elem in espec )):
     
             raise ValueError('cria_tabuleiro: argumentos invalidos')
 
-        # Qtd linhas & colunas
+        # Acede as linhas, colunas e celulas
         self.linhas = t[0]
         self.colunas = t[1]
         self.celulas = [[0 for x in range (len(self.colunas))] for y in range (len(self.linhas))]
 
-    def linhas (self):
+    # Atributo
+    def get_linhas (self):
         return self.linhas
 
-    def colunas (self):
+    # Atributo
+    def get_colunas (self):
         return self.colunas
 
-    def celulas (self):
+    # Atributo
+    def get_celulas (self):
         return self.celulas
 
+    # Igualdade
+    def __eq__ (self, tab):
+            spec1 = tabuleiro_especificacoes(self)
+            spec2 = tabuleiro_especificacoes(tab)
+            celulas1 = tabuleiro_celulas(self)
+            celulas2 = tabuleiro_celulas(tab)
+            return (spec1 == spec2 and celulas1 == celulas2)
 
 
-# t e o tuplo com as especificacoes
-def cria_tabuleiro(t):
-    return tabuleiro(t)
+# Construtor
+def cria_tabuleiro(spec):
+    '''cria_tabuleiro : tuple -> tabuleiro 
+    cria_tabuleiro(spec) recebe como argumento um tuplo composto 
+    por dois tuplos de tuplos de inteiros, em que o primeiro tuplo corresponde
+    a especificacao das linhas e o segundo a especificacao das colunas
+    cria_tabuleiro devolve um objeto do tipo tabuleiro'''
 
-# t e o tabuleiro
-def tabuleiro_especificacoes(t):
-    spec_linhas = t.linhas
-    spec_colunas = t.colunas
+    if not( isinstance(t, (tuple))
+                # Se o tuplo contem dois tuplos
+                and len(t) == 2
+                # Se os elementos do tuplo sao tuplos
+                and all( isinstance(tuplos, (tuple)) for tuplos in t )
+                # Se esses tuplos nao sao vazios
+                and all( len(tuplos) > 0 for tuplos in t )
+                # Se as especificacoes sao inteiras
+                and all( isinstance(elem, (int)) for tuplos in t for espec in tuplos for elem in espec ) 
+                # Se as especificacoes sao positivas
+                and all( (elem > 0) for tuplos in t for espec in tuplos for elem in espec )):
+    
+            raise ValueError('cria_tabuleiro: argumentos invalidos')
+    
+    return tabuleiro(spec)
+
+# Reconhecedor
+def e_tabuleiro(elem):
+    '''e_tabuleiro : universal -> logico
+    e_tabuleiro(elem) recebe um unico argumento e devolve
+    True se o argumento for do tipo tabuleiro e False caso contrario'''
+    return isinstance(elem, tabuleiro)
+
+# Seletor
+def tabuleiro_especificacoes(tab):
+    '''tabuleiro_especificacoes : tabuleiro -> tuple 
+    cria_tabuleiro(tab) recebe como argumento um objeto do tipo tabuleiro e
+    devolve um tuplo com as especificacoes do tabuleiro '''
+
+    # Testa se o argumento e do tipo tabuleiro
+    if not e_tabuleiro(tab):
+        raise ValueError('tabuleiro_especificacoes: argumentos invalidos')
+
+    # Se for tabuleiro:
+    # Obtem as especificacoes das linhas
+    spec_linhas = tab.get_linhas()
+    # Obtem as especificacoes das colunas
+    spec_colunas = tab.get_colunas()
+    # Junta as especificacoes das linhas e das colunas num tuplo
     spec = (spec_linhas, spec_colunas)
+    # Devolve o tuplo criado
     return spec
 
-def tabuleiro_celulas(t):               # funcao opcional
-    return t.celulas
+# Seletor
+def tabuleiro_celulas(tab):
+    '''tabuleiro_celulas : tabuleiro -> list 
+    tabuleiro_celulas(tab) recebe como argumento um objeto do tipo tabuleiro e
+    devolve uma lista constituida por listas com os valores que estao 
+    em cada celula do tabuleiro'''
+    # Testa se o argumento e do tipo tabuleiro
+    if not e_tabuleiro(tab):
+        raise ValueError('tabuleiro_celulas: argumentos invalidos')
+   
+    # Se for tabuleiro:
+    # Obtem a lista com os valores das celulas
+    celulas = tab.get_celulas()
+    # Devolve a lista obtida
+    return celulas
 
-# t e o tabuleiro
-def tabuleiro_dimensoes(t):
-    # Vai buscar as especificacoes do tabuleiro
-    # spec vai ser um tuplo com as especificacoes das linhas na
-    # primeira entrada e com especificacoes das colunas na segunda entrada
-    spec = tabuleiro_especificacoes(t)
+# Seletor
+def tabuleiro_celulas_linha(tab, line):
+    '''tabuleiro_celulas_linha : tabuleiro x int -> list
+    tabuleiro_celulas_linha(tab, line) recebe como argumentos um objeto do 
+    tipo tabuleiro e um argumeno do tipo inteiro correspondente a uma linha
+     e devolve uma lista com os valores das celulas da linha'''
+    
+    # Testa se o argumento tab e do tipo tabuleiro
+    if not e_tabuleiro(tab)
+        raise ValueError('tabuleiro_celulas_linha: tabuleiro invalido')
 
-    # Vai buscar o tamanho das especificacoes das linhas
-    dim_linhas = len(spec[0])
+    # Testa se o argumento line e do tipo int    
+    if not (isinstance(line, int)):
+        raise ValueError('tabuleiro_celulas_linha: linha invalida')
 
-    # Vai buscar o tamanho das especificacoes das linhas
-    dim_colunas = len(spec[1])
+    # Testa se a linha introduzida esta contida no tabuleiro indicado
+    dimensoes = tabuleiro_dimensoes(tab)
+    qtd_linhas = dimensoes[0]
+    if not (line => 0)
+        and ((qtd_linhas - 1) => line):
+        raise ValueError('tabuleiro_celulas_linha: linha fora do tabuleiro')
 
-    # Cria um tuplo com as dimensoes
-    dim = (dim_linhas, dim_colunas)
+    # Se os argumentos introduzidos sao validos:
+    # Obtem os valores de todas as celulas do tabuleiro
+    celulas = tabuleiro_celulas (tab)
+    # Dos valores obtidos, obtem os valores das celulas da linha selecionada
+    valores_linha = celulas[line]
+    # Devolve a lista com os valores obtidos
+    return preenchimento_linha
 
-    return dim
+# Seletor
+def tabuleiro_dimensoes(tab):
+    '''tabuleiro_dimensoes : tabuleiro -> tuple
+    tabuleiro_dimensoes(tab) recebe como argumento um objeto do
+    tipo tabuleiro e devolve um tuplo constituido por dois
+    inteiros com o numero de linhas no primeiro elemento e 
+    com o numero de colunas no segundo'''
+    # Testa se o argumento tab e do tipo tabuleiro
+    if not e_tabuleiro(tab)
+        raise ValueError('tabuleiro_celulas_linha: tabuleiro invalido')
+
+    spec = tabuleiro_especificacoes(tab)
+    dimensao_linhas = len(spec[0])
+    dimensao_colunas = len(spec[1])
+    dimensoes = (dimensao_linhas, dimensao_colunas)
+    return dimensoes
 
 # t e o tabuleiro, c e a coordenada
-def tabuleiro_celula(t, c):
-    linha = coordenada_linha(c)
-    coluna = coordenada_coluna(c)
-    celulas = tabuleiro_celulas(t)
+def tabuleiro_celula(tab, coord):
+    linha = coordenada_linha(coord)
+    coluna = coordenada_coluna(coord)
+    celulas = tabuleiro_celulas(tab)
     valor_celula = celulas[linha[coluna]]
 
     return valor_celula
 
 # t e o tabuleiro, c e a coordenada, e e o valor a preencher
-def tabuleiro_preenche_celula(t, c, e):
-    linha = coordenada_linha(c)
-    coluna = coordenada_coluna(c)
-    celulas = tabuleiro_celulas(t)
-    celulas = e
+def tabuleiro_preenche_celula(tab, coord, value):
+    linha = coordenada_linha(coord)
+    coluna = coordenada_coluna(coord)
+   # tabuleiro_celulas(tab) = value
     return t
 
-def e_tabuleiro(t):
-    return isinstance(t, tabuleiro)
 
-# def tabuleiro_completo
 
-def tabuleiros_iguais(t1, t2):
+def tabuleiro_completo(tab):
+    celulas_vazias = tabuleiro_celulas_vazias(tab)
+    if celulas_vazias != []:
+        return False
+    dimensoes = tabuleiro_dimensoes (tab)
+    qtd_linha = dimensoes[0]
+    especificacoes = tabuleiro_especificacoes (tab)
+    especificacoes_linhas = especificacoes[0]
+
+    for linha in range (len(qtd_linha)):
+        espec_linha = especificacoes_linhas[linha]
+        conteudo_linha = tabuleiro_celulas_linha(tab, linha)
+
+        if not linha_completa(espec_linha, conteudo_linha):
+            return False
+
+    return True
+
+def tabuleiros_iguais(tab1, tab2):
     spec1 = tabuleiro_especificacoes(t1)
     spec2 = tabuleiro_especificacoes(t2)
     celulas1 = tabuleiro_celulas(t1)
     celulas2 = tabuleiro_celulas(t2)
     return (spec1 == spec2 and celulas1 == celulas2)
 
+
+def tabuleiro_celulas_vazias(tab):
+    celulas = tabuleiro_celulas(tab)
+    dimensoes = tabuleiro_dimensoes(tab)
+    qtd_linhas = dimensoes[0]
+    qtd_colunas = dimensoes[1]
+    celulas_vazias = []
+    for linha in range (qtd_linhas):
+        for coluna in range (qtd_colunas):
+            conteudo = celulas[linha][coluna]
+            print (conteudo)
+            if conteudo == 0:
+                coord = coordenada(linha + 1, coordenada + 1)
+                celulas_vazias.append (coord)
+    return celulas_vazias
